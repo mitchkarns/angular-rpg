9d8957e8a4032ed0f2a91796fbeec0a1d75be170
Mitch Karns <mitch.karns@tubemogul.com>
2015-12-04 09:56:03 -0800

    updated something....

--- gameController.js
+++ gameController.js
@@ -8,17 +8,34 @@ angular.module('Game', []).controller('gameCtrl', function($scope, $interval) {
 	$scope.stam_max = 20;
 	$scope.stam_cur = 0;
 	$scope.hp = 100;
+	$scope.hp_max = 100;
 	$scope.level = 1;
 	$scope.percent_gain = 25;
 
-	function recover(){
-	if ($scope.stam_cur < $scope.stam_max){
-		$scope.stam_cur = $scope.stam_cur + $scope.rcv;
-		if ($scope.stam_cur > $scope.stam_max)
-			$scope.stam_cur = $scope.stam_max;
+	function updateExp(e){
+		$scope.exp += e;
+		if ($scope.exp >= $scope.exp_thresh){
+			$scope.exp -= $scope.exp_thresh;
+			$scope.level += 1;
+			$scope.exp_thresh += Math.floor($scope.level * $scope.level / 20);
 		}
 	}
 
+	function recover(){
+		if ($scope.hp < $scope.hp_max){
+			$scope.hp += $scope.rcv;
+			if ($scope.hp > $scope.hp_max)
+				$scope.hp = $scope.hp_max;
+		}
+		
+		if ($scope.stam_cur < $scope.stam_max){
+			$scope.stam_cur = $scope.stam_cur + $scope.rcv;
+			if ($scope.stam_cur > $scope.stam_max)
+				$scope.stam_cur = $scope.stam_max;
+		}
+	}
+
+	//every 100 mils this should update hp and stam
 	function update(){
 		recover();
 	}
